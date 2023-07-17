23ba1966e42a0794650538ad15d137d29ed131b7
shruthi <shruthisa.17@gmail.com>
2023-07-16 23:29:57 -0700

    final commit

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,6 +1,15 @@
 // IMPORT THE MODULE
+import { calculate, addNumbers, subtractNumbers, multiplyNumbers, divideNumbers } from "./modules/calculator.js";
+function main() {
 
-// COLLECT NUMBER 1, NUMBER 2, AND OPERATION FROM THE USER
+        // COLLECT NUMBER 1, NUMBER 2, AND OPERATION FROM THE USER
+        var number1 = Number(window.prompt("Please enter the first number\n"));
+        var number2 = Number(window.prompt("Please enter the second number\n"));
+    
+        // CHECK TO SEE WHAT OPERATION THEY'RE USING
+        var operation = window.prompt("Please enter + or - or * or / ");
+        calculate(number1, number2, operation);
 
-// CHECK TO SEE WHAT OPERATION THEY'RE USING
-// CALL THE APPROPRIATE FUNCTION
+}
+
+main();
--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -1,13 +1,67 @@
+
 // ADD A PRIVATE FUNCTION CALLED CALCULATE
+function calculate(number1, number2, operation) {
+
+    // CALL THE APPROPRIATE FUNCTION
+    switch(operation) {
+         case "+" : addNumbers(number1, number2);
+                break;
+         case "-" : subtractNumbers(number1, number2);
+                break;
+         case "*" : multiplyNumbers(number1, number2);
+                break;
+         case "/" : divideNumbers(number1, number2);
+                break;
+         default : window.alert("Wrong choice of operation. Try again..");
+    }
+}
+
+
 
 // ADD FOUR PUBLIC FUNCTIONS BELOW
 
 // ADD FUNCTION
+function addNumbers(number1, number2) {
+    var result;
+    result = number1 + number2;
+    console.log("The sum of the numbers is " + result);
+    return result ;
+}
 
 // SUBTRACT FUNCTION
+function subtractNumbers(number1, number2) {
+    var result;
+        if(number1 > number2) {
+           result = number1 - number2;
+        }
+        else {
+            result = number2 - number1;
+        }
+        console.log("The difference of the numbers is " + result);
+       return result ;
+ }
 
 // MULTIPLY FUNCTION
+function multiplyNumbers(number1, number2) {
+    var result;
+        result = number1 * number2;
+        console.log("The product of the numbers is " + result);
+       return result ;
+}
 
 // DIVIDE FUNCTION
+function divideNumbers(number1, number2) {
+    var result;
+        if(number2 != 0) {
+            result = number1 / number2;
+            console.log("The quotient of the numbers is " + result);
+           return result ;
+        }
+        else {
+            window.alert("The divisor cannot be 0");
+        }
+      
+}
 
 // EXPORT THE FOUR PUBLIC FUNCTIONS
+export {calculate, addNumbers, subtractNumbers, multiplyNumbers, divideNumbers};
